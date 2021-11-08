83c3c532436ad3468b5615ea3e1813329cb643d6
tuptajacy-noca-jez <krystian7891@gmail.com>
2021-11-08 17:04:38 +0100

    Head template

---
+++ Market365-3.0/shared/_Head.aspx.designer.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Market365_3._0.shared {
+
+
+    public partial class _Head {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
