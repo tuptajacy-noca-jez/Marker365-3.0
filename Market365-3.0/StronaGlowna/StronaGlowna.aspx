9c4d40f20ff1821666e60e9ca53cf9d8b9b92df8
Marcin Brzeziński <marcin.brzezinski01@student.wat.edu.pl>
2021-11-13 12:14:22 +0100

    Strona Główna

---
+++ Market365-3.0/StronaGlowna/StronaGlowna.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StronaGlowna.aspx.cs" Inherits="Market365_3._0.StronaGlowna.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="/Style/StyleGadomski.css" rel="stylesheet" />
+    <link href="/Style/StyleBrzezinski.css" rel="stylesheet" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <div>
+        <form id="form1" runat="server">
+            <div class="header">
+                <table>
+                    <tr>
+                        <td class="tdhead">
+
+                        </td>
+                        <td class="tdhead">
+                            <div class="namep">
+                                <asp:Label ID="Label1" runat="server" Text="Market365"></asp:Label>
+                            </div>
+                        </td>
+                        <td class="tdhead">
+                            <div class ="buttonLogout">
+                                <asp:Button CssClass="buttonL" ID="wyloguj" runat="server" Text="Wyloguj"/>
+                            </div>
+
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+        </form>
+
+    </div>
+</body>
+</html>
---
+++ Market365-3.0/StronaGlowna/StronaGlowna.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generowane automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Zmiany w tym pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowane automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Market365_3._0.StronaGlowna
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka Label1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Kontrolka wyloguj.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button wyloguj;
+    }
+}
