065e23efe097886a76ec3cf4fd55e4bf93c68cf0
Lasermistrz <74707568+Lasermistrz@users.noreply.github.com>
2021-11-27 18:30:41 +0100

    Modyfikacja bazy danych, testowe wyswietlenie danych z bazy (obrazy w bazie zapisywane jako base64)

--- Market365-3.0/Koszyk/Koszyk.aspx.cs
+++ Market365-3.0/Koszyk/Koszyk.aspx.cs
@@ -18,5 +18,13 @@ namespace Market365_3._0.Koszyk
         {
             Response.Redirect("~/stronaGłówna/stronaGłówna.aspx");
         }
+
+        protected static string ReturnEncodedBase64UTF8(object rawImg)
+        {
+            string img = "data:image/jpg;base64,{0}";
+            byte[] toEncodeAsBytes = (byte[])rawImg;
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            return String.Format(img, returnValue);
+        }
     }
 }
