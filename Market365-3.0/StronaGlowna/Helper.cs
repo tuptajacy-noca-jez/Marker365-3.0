0f256e49a82eae70b9225cfc86860c4bb8b66b94
MaciejSieczka <maciej.sieczka1@gmail.com>
2021-12-01 22:56:56 +0100

    Api commit

---
+++ Market365-3.0/StronaGlowna/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http.Headers;
+using System.Net.Http;
+
+namespace Market365_3._0.StronaGlowna
+{
+    public class Helper
+    {
+        public static HttpClient apiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            apiClient = new HttpClient();
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
--- Market365-3.0/StronaGlowna/StronaGlowna.aspx.cs
+++ Market365-3.0/StronaGlowna/StronaGlowna.aspx.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
+
 
 namespace Market365_3._0.StronaGlowna
 {
     public partial class StronaGlowna : System.Web.UI.Page {
 
         User currUser;
+
+        
         
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            RegisterAsyncTask(new PageAsyncTask(getRandomDrink));
         }
 
         protected void sklep_Click(object sender, EventArgs e)
@@ -64,6 +69,26 @@ namespace Market365_3._0.StronaGlowna
                 //Server.Transfer("~/MojProfil/MojProfil.aspx");
                 Response.Redirect("~/MojProfil/MojProfil.aspx");
             }
+        }
+
+        private async Task getRandomDrink()
+        {
+            DrinkList.Items.Clear();
+            Helper.InitializeClient();
+            var drink = await DrinkProcessor.LoadDrink();
+            DrinkImage.ImageUrl = drink.strDrinkThumb;
+            DrinkName.Text = drink.strDrink;
+            if (drink.strIngredient1 != null)
+                DrinkList.Items.Add(drink.strIngredient1);
+            if (drink.strIngredient2 != null)
+                DrinkList.Items.Add(drink.strIngredient2);
+            if (drink.strIngredient3 != null)
+                DrinkList.Items.Add(drink.strIngredient3);
+            if (drink.strIngredient4 != null)
+                DrinkList.Items.Add(drink.strIngredient4);
+            if (drink.strIngredient5 != null)
+                DrinkList.Items.Add(drink.strIngredient5);
+            DrinkInstructions.Text = drink.strInstructions;
         }
 
         /*
